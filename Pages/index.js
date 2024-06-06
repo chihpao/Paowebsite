4adf58084f34a057e2c72ec1b976935f7e527072
chihpao <chihpaoo@gmail.com>
2024-06-06 13:05:10 +0800

    更新操作說明並增加動畫互動功能

--- Pages/index.js
+++ Pages/index.js
@@ -7,7 +7,13 @@ const P5Wrapper = dynamic(() => import('../components/P5Wrapper'), { ssr: false 
 export default function Home() {
   return (
     <MainLayout>
-      <h1>asdd!</h1>
+      <h1>動畫演示</h1>
+      <p>操作說明：</p>
+      <ul>
+        <li>按下 <b>'c'</b> 鍵可以改變粒子顏色。</li>
+        <li>按下 <b>'s'</b> 鍵可以改變粒子速度。</li>
+        <li>點擊滑鼠可以在點擊位置新增粒子。</li>
+      </ul>
       <P5Wrapper />
     </MainLayout>
   );
--- components/P5Wrapper.js
+++ components/P5Wrapper.js
@@ -1,41 +1,100 @@
 import React, { useRef, useEffect } from 'react';
-import p5 from 'p5';
+import dynamic from 'next/dynamic';
 
 const P5Wrapper = () => {
   const sketchRef = useRef();
 
   useEffect(() => {
-    const sketch = (p) => {
-      let x = 100;
-      let y = 100;
-      let direction = 1;
+    if (typeof window !== 'undefined') {
+      const p5 = require('p5');
 
-      p.setup = () => {
-        p.createCanvas(800, 600);
-      };
+      const sketch = (p) => {
+        let particles = [];
+        let numParticles = 100;
+        let particleColor = 'rgba(200,169,169,0.5)';
+        let particleSpeed = 1;
 
-      p.draw = () => {
-        p.background(200);
-        p.fill(50);
-        p.noStroke();
-        p.ellipse(x, y, 50, 50);
+        class Particle {
+          constructor(x, y) {
+            this.x = x;
+            this.y = y;
+            this.r = p.random(5, 15);
+            this.xSpeed = p.random(-2, 2) * particleSpeed;
+            this.ySpeed = p.random(-1, 1.5) * particleSpeed;
+          }
 
-        x += 2 * direction;
-        if (x > p.width - 25 || x < 25) {
-          direction *= -1;
+          createParticle() {
+            p.noStroke();
+            p.fill(particleColor);
+            p.ellipse(this.x, this.y, this.r);
+          }
+
+          moveParticle() {
+            if (this.x < 0 || this.x > p.width)
+              this.xSpeed *= -1;
+            if (this.y < 0 || this.y > p.height)
+              this.ySpeed *= -1;
+            this.x += this.xSpeed;
+            this.y += this.ySpeed;
+          }
+
+          joinParticles(particles) {
+            particles.forEach(element => {
+              let dis = p.dist(this.x, this.y, element.x, element.y);
+              if (dis < 85) {
+                p.stroke('rgba(255,255,255,0.04)');
+                p.line(this.x, this.y, element.x, element.y);
+              }
+            });
+          }
         }
+
+        p.setup = () => {
+          p.createCanvas(800, 600);
+          for (let i = 0; i < numParticles; i++) {
+            particles.push(new Particle(p.random(p.width), p.random(p.height)));
+          }
+        };
+
+        p.draw = () => {
+          p.background(0);
+          for (let i = 0; i < numParticles; i++) {
+            particles[i].createParticle();
+            particles[i].moveParticle();
+            particles[i].joinParticles(particles.slice(i));
+          }
+        };
+
+        p.mousePressed = () => {
+          particles.push(new Particle(p.mouseX, p.mouseY));
+        };
+
+        p.keyPressed = () => {
+          if (p.key === 'c') {
+            // Change particle color
+            particleColor = `rgba(${p.random(255)},${p.random(255)},${p.random(255)},0.5)`;
+          } else if (p.key === 's') {
+            // Change particle speed
+            particleSpeed = p.random(0.5, 2);
+            particles.forEach(particle => {
+              particle.xSpeed = p.random(-2, 2) * particleSpeed;
+              particle.ySpeed = p.random(-1, 1.5) * particleSpeed;
+            });
+          }
+        };
       };
-    };
 
-    const p5Instance = new p5(sketch, sketchRef.current);
+      const p5Instance = new p5(sketch, sketchRef.current);
 
-    // Cleanup function to remove the p5 instance on component unmount
-    return () => {
-      p5Instance.remove();
-    };
+      // Cleanup function to remove the p5 instance on component unmount
+      return () => {
+        p5Instance.remove();
+      };
+    }
   }, []);
 
   return <div ref={sketchRef}></div>;
 };
 
-export default P5Wrapper;
+// Disable SSR for this component
+export default dynamic(() => Promise.resolve(P5Wrapper), { ssr: false });
